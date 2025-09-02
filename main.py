5cd4c10b537eac8b248b3db48d158cc4074a283a
Ignacio Sota <sota.123198@e.unavarra.es>
2025-09-02 22:46:13 +0200

    [8] Main

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+from ui.interface import run_interface
+
+def main():
+    print("Iniciando aplicación de generación de vídeo...")
+    run_interface()
+
+if __name__ == "__main__":
+    main()
+
+
